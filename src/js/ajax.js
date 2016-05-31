2a3c8218b68e77db7c50bcd9ad98e8f393deb1a7
hjj9619 <hjj9619@163.com>
2016-05-31 16:09:23 +0800

    Update ajax.js

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -1,102 +1,38 @@
 /* 此项目创建于2016/5/12 */
-window.onload = function () {
-
-
-
-
-function ajax(method,url,async, fn){
-
-  //创建一个Ajax对象
-  var ajax = new XMLHttpRequest() || ActiveXObject("Microsoft.XMLHTTP");
-
-
-
-  //选择方式提交到url,并选择是否异步
-  ajax.open(method, url +"?"+ new Date().getTime(), async);
-
-  //提交
-  ajax.send();
-
-  //当状态值发生改变的时候
-  ajax.onreadystatechange = function(){
-    if( ajax.readyState == 4 && ajax.status == 200){
-      fn(ajax.responseText);
-    }
-  }
-
-}
-
-function get(url, fn){
-    var ajax = new XMLHttpRequest() || new ActiveXObject("Microsoft.XMLHTTP");
-
-    ajax.open("get", url +"?"+ new Date().getTime(), true);
-
-    ajax.send();
-
-    ajax.onreadystatechange = function(){
-      if(ajax.readyState == 4 && ajax.status == 200){
-        fn(ajax.responseText);
-      }
-    }
-}
-
-function post(url, fn){
-    var ajax = new XMLHttpRequest(); //|| ActiveXObject("Microsoft.XMLHTTP");
-
-
-    ajax.open("post", url + "?" + new Date().getTime(), true);
-
-    ajax.send();
-
-    ajax.onreadystatechange = function(){
-      if(ajax.readyState == 4 && ajax.status == 200){
-        fn(ajax.responseText);
-      }
-    }
-}
-
-
-}
-
-
 
 function Ajax(){
-  this.get = function(url, fn){
+  this.ajax;
+  if(window.XMLHttpRequest){
+  	this.ajax = new XMLHttpRequest();
+  }else{
+  	this.ajax = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+}
 
-    var ajax = new XMLHttpRequest() || new ActiveXObject("Microsoft.XMLHTTP");
 
-    ajax.open("get", url + "?" + new Date().getTime(), true);
-
-    ajax.send();
-
-    ajax.onreadystatechange = function(){
-
-      if(ajax.readyState == 4 && ajax.status == 200){
-        fn(ajax.responseText);
-      }
+Ajax.prototype.post = function(url, mess, fn){
+  var _this = this;
+  _this.ajax.open("post", url, true);
+  _this.ajax.setRequestHeader("CONTENT-TYPE", "application/x-www-form-urlencoded");
+  _this.ajax.send(mess);
+  ajax.onreadystatechange = function(){
+    if(_this.ajax.readyState == 4 && _this.ajax.status == 200){
+      fn(_this.ajax.responseText);
     }
   }
-
-
-  this.post = function(url, fn){
-    var ajax = new XMLHttpRequest() || new ActiveXObject("Microsoft.XMLHTTP");
-    ajax.open("post", url + "?" + new Date().getTime(), true);
-    ajax.send();
-    ajax.onreadystatechange = function(){
-      if(ajax.readyState == 4 && ajax.status == 200){
-        fn(ajax.responseText);
-      }
+}
+Ajax.prototype.get = function(url, fn){
+  var _this = this;
+  _this.ajax.open("get", url + "?" + new Date().getTime(), true);
+  _this.ajax.send();
+  _this.ajax.onreadystatechange = function(){
+    if(_this.ajax.readyState == 4 && _this.ajax.status == 200){
+      fn(_this.ajax.responseText);
     }
   }
 }
 
 
-var ajax = new Ajax();
-document.onclick = function(){
-  ajax.post("js/test.txt", function(txt){
-    alert(txt);
-  });
-}
 
 
 
